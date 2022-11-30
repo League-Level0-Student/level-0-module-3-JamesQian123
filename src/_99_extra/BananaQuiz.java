6cf55e5510560b6b9c38de4d5f0071f7d1ed61d7
league <league@imac-18.attlocal.net>
2022-11-30 15:55:29 -0800

    asf

--- src/_99_extra/BananaQuiz.java
+++ src/_99_extra/BananaQuiz.java
@@ -9,7 +9,16 @@ public class BananaQuiz
 	public static void main(String[] args)
 	{
 		//1. ask the user if they like bananas
+		String like = JOptionPane.showInputDialog("Do you like bananas");
 		//2. if they say no, 
+		if(like .equalsIgnoreCase("no")) {
+			JOptionPane.showMessageDialog(null, "You are crazy");
+		}
+		else if(like .equalsIgnoreCase("yes")) {
+			String hobby = JOptionPane.showInputDialog("What is your favorite hobby");
+			JOptionPane.showMessageDialog(null, hobby + " is much better with bananas");
+		}
+		
 			//tell them they are crazy 
 			//and end quiz
 		//3. if they say yes
--- src/_99_extra/BirthdayReminder.java
+++ src/_99_extra/BirthdayReminder.java
@@ -8,21 +8,32 @@ public class BirthdayReminder {
 	public static void main(String[] args) {
 	
 		// 1. correct the birthdays for your family below
-		String momsBirthday = "January 24th";
-		String dadsBirthday = "June 9th";
-		String myBirthday = "April 16th";
+		String momsBirthday = "Febuary 18th";
+		String dadsBirthday = "October 12th";
+		String myBirthday = "December 3rd";
 
 		// 2. Find out which birthday the user wants and and store their response in a variable
-	
+		String birthday = JOptionPane.showInputDialog("Which birthday do you want ");
 		// 3. Print out what the user typed
-		
+		System.out.println(birthday);
 		// 4. if user asked for "mom"
+		if(birthday .equals("mom")) {
+			System.out.println(momsBirthday);
+		}
 			//print mom's birthday
 		// 5. if user asked for "dad"
+		else if(birthday .equals("dad")) {
+			System.out.println(dadsBirthday);
+		}
 			// print dad's birthday
 		// 6. if user asked for your name
 			// print myBirthday
+		else if(birthday .equals("james")) {
+			System.out.println(myBirthday);
+		}
 		//7. otherwise print "Sorry, i don't remember that person's birthday!"
-
+		else {
+			System.out.println("Sorry, I don't remeber that person's birthday!");
+		}
 	} 
 }
